4a830aacfa50628565fa26cb7920329897b936e1
Ahmad k. AlMahasneh <ahmadkhaleel96@yahoo.com>
2022-12-21 12:16:37 +0300

    Changed the Vision Background Color

--- manzel/src/components/Vision/visionElements.js
+++ manzel/src/components/Vision/visionElements.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const VisionContainer = styled.div`
   color: #fff;
-  background: ${({ lightBg }) => (lightBg ? "#f9f9f9" : "#010606")};
+  background: ${({ lightBg }) => (lightBg ? "#f9f9f9" : "#02233C")};
 
   @media screen and (max-width: 768px) {
     padding: 100px 0;
